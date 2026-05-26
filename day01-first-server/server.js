825b493fff4b048b1dfc86fe2f29628930de7344
Shruti Hadkar <shrutihadkar82@gmail.com>
2026-05-26 11:51:48 +0530

    Day 1 challenge: Add about route

--- day01-first-server/server.js
+++ day01-first-server/server.js
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.send('hello! my server is working');
 });
 
+app.get('/about', (req,res) => {
+    res.send('my name is shruti and i am building backend api');
+});
+
 app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
 });
